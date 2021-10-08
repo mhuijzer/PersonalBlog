c2296d32ae4cfa8036e15437ce0ac53f762f9688
Matthijs Huijzer <matthijs.huijzer@live.nl>
2021-10-08 16:08:13 +0200

    Massive update, from single blogpost to full site

--- core.js
+++ core.js
@@ -8,3 +8,13 @@ while (references.item(i) != null){
     }
     i++;
 }
+previous = document.getElementsByTagName('previous');
+
+j = 0;
+
+while (previous.item(j) != null){
+    previous.item(j).onclick = function(){
+        this.classList.toggle('opened');
+    }
+    j++;
+}
